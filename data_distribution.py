1845b250821b0b32f68f5cd66db950bc8b3d6721
Albert Pangilinan <rapangil@uwaterloo.ca>
2022-10-05 17:25:46 -0400

    initial commit (data distribution)

---
+++ data_distribution.py
@@ -0,0 +1,13 @@
+# SEP 740 Final Project - Aerial Perspective Object Detection
+
+# Authors: 
+#   Jukai Hu (400485702)
+#   Ray Albert Pangilinan (400065058)
+#   Luke Vanden Broek (400486889)
+
+# Data Distribution
+
+
+import matplotlib. pyplot as plt
+import numpy as np
+
