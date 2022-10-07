b36dedb6f64bd1e8917360959990f287fa0a841b
Albert Pangilinan <rapangil@uwaterloo.ca>
2022-10-07 14:32:07 -0400

    changed iage to 000.png, added colour legend and comments

--- data_distribution.py
+++ data_distribution.py
@@ -5,7 +5,9 @@
 #   Ray Albert Pangilinan (400065058)
 #   Luke Vanden Broek (400486889)
 
-# Data Distribution (113.png)
+# Data Distribution (000.png)
+
+# Imports
 
 import os.path
 cwd = os.getcwd()
@@ -15,6 +17,8 @@ import matplotlib.image as image
 import numpy as np
 import pandas as pd
 
+# Reading color image mask files from disk
+
 labels_dir = cwd + "/../dataset/class_dict_seg.csv"
 masks_dir = cwd + "/../dataset/RGB_color_image_masks/RGB_color_image_masks/"
 
@@ -23,10 +27,28 @@ rgb_arr = distribution.to_numpy()[:, 1:]
 distribution.insert(4, "count", 0)
 
 masks_list = os.listdir(masks_dir)
+masks_list.sort()
 mask_0_filename = masks_list[0]
 
 masks_list = list(map(lambda mask : masks_dir + mask, masks_list))
 mask_0_rgb = np.array(image.imread(masks_list[0])) * 255
+
+# Plotting colour legend
+
+labels = distribution.to_numpy()[:, :1].flatten()
+rgb_tuples = list(map(lambda col : tuple(col / 255), rgb_arr))
+
+fig, axs = plt.subplots(6, 4)
+
+for i, ax in enumerate(fig.axes):
+  ax.imshow([[rgb_tuples[i]]])
+  ax.set_title(labels[i])
+  ax.axis("off")
+
+fig.suptitle("Colour Legend")
+plt.show()
+
+# Counting pixel distribution
 
 for row in mask_0_rgb:
   for pixel in row[:1]:
@@ -34,8 +56,8 @@ for row in mask_0_rgb:
     distribution.at[i[0], "count"] += 1
 
 print(distribution)
-labels = distribution.to_numpy()[:, :1].flatten()
-rgb_tuples = list(map(lambda col : tuple(col / 255), rgb_arr))
+
+# Plotting pixel distribution
 
 plt.bar(range(len(labels)), distribution.loc[:, "count"], color=rgb_tuples)
 plt.title("Pixel Class Distribution for " + mask_0_filename)
